0a7d5d11ae8e5332b43ee064c6be5e76874f50c9
Dirane <diranengala@gmail.com>
2023-09-05 00:04:59 +0100

    Working with props, passing down through components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
+  const subject = props.subject
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello, World!</p>
+        <p>Hello, {subject} !</p>
       </header>
     </div>
   );
