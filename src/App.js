3437b7545c17bc1b5e1005b1d22900dacb23e562
Dirane <diranengala@gmail.com>
2023-09-06 15:01:05 +0100

    Separating into components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Todo from "./components/Todo";
+
 function App(props) {
   const subject = props.subject
   return (
@@ -12,6 +14,7 @@ function App(props) {
         <input
         type="text"
         id="new-todo-input"
+        className="input input__lg"
         name="text"
         autoComplete="off"
         />
@@ -41,54 +44,9 @@ function App(props) {
         role="list"
         className="todo-list stack-large stack-exception"
         aria-labelledby="list-heading">
-          <li className="todo stack-small">
-            <div className="c-cb">
-              <input id="todo-0" type="checkbox" defaultChecked={true} />
-              <label className="todo-label" htmlFor="todo-0">
-                Eat
-              </label>
-            </div>
-            <div className="btn-group">
-              <button type="button" className="btn">
-                Edit <span className="visually-hidden">Eat</span>
-              </button>
-              <button type="button" className="btn btn__danger">
-                Delete <span className="visually-hidden">Eat</span>
-              </button>
-            </div>
-          </li>
-          <li className="todo stack-small">
-            <div className="c-cb">
-              <input id="todo-1" type="checkbox" />
-              <label className="todo-label" htmlFor="todo-1">
-                Sleep
-              </label>
-            </div>
-            <div className="btn-group">
-              <button type="button" className="btn">
-                Edit <span className="visually-hidden">Sleep</span>
-              </button>
-              <button type="button" className="btn btn__danger">
-                Delete <span className="visually-hidden">Sleep</span>
-              </button>
-            </div>
-          </li>
-          <li className="todo stack-small">
-          <div className="c-cb">
-            <input id="todo-2" type="checkbox" />
-            <label className="todo-label" htmlFor="todo-2">
-              Repeat
-            </label>
-          </div>
-          <div className="btn-group">
-            <button type="button" className="btn">
-              Edit <span className="visually-hidden">Repeat</span>
-            </button>
-            <button type="button" className="btn btn__danger">
-              Delete <span className="visually-hidden">Repeat</span>
-            </button>
-          </div>
-        </li>
+         <Todo />
+         <Todo />
+         <Todo />
         </ul>
     </div>
   );
